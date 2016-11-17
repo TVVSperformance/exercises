cdf0af39a780d739df5f41f7a50090c5544a3d75
Paulo Barbosa[1130648] <1130648@isep.ipp.pt>
2016-11-17 19:12:57 +0000

    Project.

---
+++ PerformanceTesting/src/frequentelements/ITopIKFrequentElements.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package frequentelements;
+
+import java.util.List;
+
+/**
+ *
+ * @author Paulo
+ */
+public interface ITopIKFrequentElements {
+
+	public List<Integer> topKFrequent(int[] nums, int k);
+
+}
---
+++ PerformanceTesting/src/primecounter/IPrimeCounter.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primecounter;
+
+/**
+ *
+ * @author Paulo
+ */
+public interface IPrimeCounter {
+
+	// Count the number of prime numbers less than a non-negative number, n
+	public int countPrimes(int n);
+
+}
